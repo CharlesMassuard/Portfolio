c3df7e0ba0e713304fb3ba8f8cca869c2ac355f4
CharlesMassuard <charles.massuard@etu.univ-orleans.fr>
2024-05-29 19:50:56 +0200

    Fix animation pour tout

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var text1 = document.querySelector('.animated-text');
-    text1.innerHTML = text1.textContent.replace(/\S/g, function(letter, index) {
-        return `<span class='letter' style='--i: ${index}'>${letter}</span>`;
-    });
-
-    var text2 = document.querySelector('.animated-text-bottom');
-    var text2Content = text2.textContent;
-    text2.textContent = '';
-
-    var duration = text1.textContent.length * 0.5;
-
-    setTimeout(function() {
-        text2.innerHTML = text2Content.replace(/\S/g, function(letter, index) {
+    var texts1 = document.querySelectorAll('.animated-text');
+    texts1.forEach(function(text1) {
+        text1.innerHTML = text1.textContent.replace(/\S/g, function(letter, index) {
             return `<span class='letter' style='--i: ${index}'>${letter}</span>`;
         });
-    }, duration * 70);
+    });
+
+    var texts2 = document.querySelectorAll('.animated-text-bottom');
+    texts2.forEach(function(text2) {
+        var text2Content = text2.textContent;
+        text2.textContent = '';
+
+        var duration = text2.textContent.length * 0.5;
+
+        setTimeout(function() {
+            text2.innerHTML = text2Content.replace(/\S/g, function(letter, index) {
+                return `<span class='letter' style='--i: ${index}'>${letter}</span>`;
+            });
+        }, duration * 70);
+    });
 });
