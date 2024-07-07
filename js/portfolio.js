02672bb27bf76a372f712758f22953d7a5b9ebba
CharlesMassuard <charles.massuard@etu.univ-orleans.fr>
2024-07-07 14:39:03 +0200

    CSS portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,23 @@
-async function fetchProjets() {
+async function fetchAndDisplayProjets() {
     try {
         const response = await fetch("../BD/portfolio.json");
-        const json = await response.json();
-        console.log(json);
-        for (let i = 0; i < json.length; i++) {
-            console.log(json[i]);
-        }
-        
-        return json;
+        const data = await response.json();
+        const gridProjet = document.getElementById("gridProjets");
+
+        data.projets.forEach(projet => {
+            const projetDiv = document.createElement("div");
+            projetDiv.className = "projet";
+            projetDiv.innerHTML = `
+                <img src="${projet.img}" alt="Image du projet" title="Voir le projet">
+                <h2 title="Voir le projet">${projet.nom}</h2>
+            `;
+            gridProjet.appendChild(projetDiv);
+        });
     } catch (error) {
         console.error('Error fetching projects', error);
     }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    fetchProjets();
+    fetchAndDisplayProjets();
 });
