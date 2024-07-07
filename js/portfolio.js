fc3b28875fdfe5daacf395ff1231d2306d18793f
CharlesMassuard <charles.massuard@etu.univ-orleans.fr>
2024-07-07 14:23:23 +0200

    test fetch netlify

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,7 +2,7 @@ async function fetchProjets() {
     try {
         const response = await fetch("../BD/portfolio.json");
         const json = await response.json();
-        
+        console.log(json);
         for (let i = 0; i < json.length; i++) {
             console.log(json[i]);
         }
